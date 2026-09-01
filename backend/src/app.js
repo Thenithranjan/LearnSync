52120d996a0f11397990c2c7ef7ca49a882fd5b8
Thenith Ranjan <thenithranjan@gmail.com>
2026-09-01 12:16:14 +0530

    feat: register analytics routes in express app

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,7 @@ const progressRoutes = require('./routes/progress.routes');
 const assessmentRoutes = require('./routes/assessment.routes');
 const attendanceRoutes = require('./routes/attendance.routes');
 const forumRoutes = require('./routes/forum.routes');
+const analyticsRoutes = require('./routes/analytics.routes');
 const testRoutes = require('./routes/test.routes');
 const errorHandler = require('./middleware/error.middleware');
 const { sendError } = require('./utils/apiResponse');
@@ -62,6 +63,7 @@ app.use('/api', progressRoutes);
 app.use('/api', assessmentRoutes);
 app.use('/api', attendanceRoutes);
 app.use('/api', forumRoutes);
+app.use('/api', analyticsRoutes);
 app.use('/api', testRoutes);
 
 // Handle 404 for undefined routes
