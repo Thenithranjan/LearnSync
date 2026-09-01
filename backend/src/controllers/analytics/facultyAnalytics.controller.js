e378f11ed891c5cfcf9cc037e5d88970c9fbfc2f
Thenith Ranjan <thenithranjan@gmail.com>
2026-09-01 12:15:37 +0530

    feat: add faculty analytics controller

---
+++ backend/src/controllers/analytics/facultyAnalytics.controller.js
@@ -0,0 +1,16 @@
+const FacultyAnalyticsService = require('../../services/analytics/facultyAnalytics.service');
+const { sendSuccess } = require('../../utils/apiResponse');
+
+class FacultyAnalyticsController {
+  static async getOverview(req, res, next) {
+    try {
+      const facultyId = req.user._id;
+      const overview = await FacultyAnalyticsService.getFacultyOverview(facultyId);
+      return sendSuccess(res, 200, 'Faculty overview analytics retrieved', overview);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+module.exports = FacultyAnalyticsController;
