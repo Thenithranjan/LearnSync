789f3841e6550099861b044b26355e85f3e3320f
Thenith Ranjan <thenithranjan@gmail.com>
2026-09-01 12:15:48 +0530

    feat: add admin analytics controller

---
+++ backend/src/controllers/analytics/adminAnalytics.controller.js
@@ -0,0 +1,24 @@
+const AdminAnalyticsService = require('../../services/analytics/adminAnalytics.service');
+const { sendSuccess } = require('../../utils/apiResponse');
+
+class AdminAnalyticsController {
+  static async getOverview(req, res, next) {
+    try {
+      const overview = await AdminAnalyticsService.getAdminOverview();
+      return sendSuccess(res, 200, 'Institution overview analytics retrieved', overview);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async getDepartments(req, res, next) {
+    try {
+      const departments = await AdminAnalyticsService.getDepartmentAnalytics();
+      return sendSuccess(res, 200, 'Department performance analytics retrieved', departments);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+module.exports = AdminAnalyticsController;
